46d4b93b81594dcc2e8e0698fc93b1b68545316d
tinabesal <tina.besal@gmail.com>
2022-04-11 23:48:45 +0200

    adding first 5 ex

---
+++ ex04/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+// Using mili = [4, 10, 25] would be invalid
+
+const milili = [10, 25, 4];
+
+function myArr(mili) {
+    "use strict";
+    mili[0] = 4;
+    mili[1] = 10;
+    mili[2] = 25;
+
+    return mili;
+}
+console.log(myArr(milili));
+
+// Only change code above this line
+
+module.exports = myArr;
